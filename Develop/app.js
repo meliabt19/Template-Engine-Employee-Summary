b0a8d84fc2c9368ee40f7a85901900a6d1a1a358
meliabt19 <58869913+meliabt19@users.noreply.github.com>
2020-05-06 21:27:14 -0400

    mostly finished

--- Develop/app.js
+++ Develop/app.js
@@ -7,9 +7,147 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const writeFileAs = util.promisify(fs.writeFile)
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's your managers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What's is your managers id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What's your managers email?",
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "What's your managers office number?",
+      },
+    ])
+    .then(function (answers) {
+      const manager = new Manager(
+        answers.name,
+        parseInt(answers.id),
+        answers.email,
+        parseInt(answers.office)
+      );
+      employees.push(manager);
+      addMember();
+    });
+}
+
+function addMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "type",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members",
+        ],
+      },
+    ])
+    .then(function (answer) {
+      if (answer.type === "Engineer") {
+        createEngineer();
+      } else if (answer.type === "Intern") {
+        createIntern();
+      } else {
+        console.log(employees);
+        const html = render(employees);
+        return writeFileAsync("./output/team.html", html);
+      }
+    });
+}
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's your engineers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What's your engineers id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What's your engineers email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What's your engineers github?",
+      },
+    ])
+    .then(function (answers) {
+      const engineer = new Engineer(
+        answers.name,
+        parseInt(answers.id),
+        answers.email,
+        answers.github
+      );
+      employees.push(engineer);
+      addMember();
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's your interns name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What's your interns id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What's your interns email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What's your interns school?",
+      },
+    ])
+    .then(function (answers) {
+      const intern = new Intern(
+        answers.name,
+        parseInt(answers.id),
+        answers.email,
+        answers.school
+      );
+      employees.push(intern);
+      addMember();
+    });
+}
+createManager();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
